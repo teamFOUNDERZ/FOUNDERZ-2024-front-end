c31c700712fcda7f5dba6113a67b1286bc490e1c
101 <tmddnjs001133@dsm.hs.kr>
2024-11-18 12:22:54 +0900

    fix: header 추가

--- src/apis/notice.ts
+++ src/apis/notice.ts
@@ -1,4 +1,5 @@
 import { instance } from "./interceptor";
+import { EventSourcePolyfill } from "event-source-polyfill";
 
 /**
  * @returns 알림 단일 조회 response
@@ -9,10 +10,13 @@ import { instance } from "./interceptor";
  *   - content: string
  * }
  */
-export const getNoticeById = async (id: string) => {
+export const getNoticeById = async (id: string, token: string) => {
     return await instance({
         method: 'GET',
         url: `/api/notices/${id}`,
+        headers: {
+            Authorization: `Bearer ${token}`,  
+        },
     });
 };
 
@@ -27,17 +31,27 @@ export const getNoticeById = async (id: string) => {
  *   }
  * }
  */
-export const getAllNotice = async () => {
+export const getAllNotice = async (token: string) => {
     return await instance({
         method: 'GET',
         url: '/api/notices',
+        headers: {
+            Authorization: `Bearer ${token}`,  
+        },
     });
 };
 
 /**
  * @returns 알림 실시간 구독 response (sse)
  */
-export const subscribeNotice = () => {
-    const eventSource = new EventSource('/api/notices/subscribe');
+export const subscribeNotice = (token: string) => {
+    const eventSource = new EventSourcePolyfill(
+        '/api/notices/subscribe',
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,  
+            }
+        }
+    );
     return eventSource;
 };
